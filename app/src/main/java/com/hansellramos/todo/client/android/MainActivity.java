0bd4af970e6ad5602c928f1f525de161efcbc5df
Hansell Ramos <hansell.x.ramos.-nd@disney.com>
2017-08-25 13:08:44 -0500

    Added firebase authentication

--- app/src/main/java/com/hansellramos/todo/client/android/MainActivity.java
+++ app/src/main/java/com/hansellramos/todo/client/android/MainActivity.java
@@ -1,13 +1,71 @@
 package com.hansellramos.todo.client.android;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnSignOut = (Button) findViewById(R.id.btnSignOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFirebaseAuth != null) {
+                    mFirebaseAuth.signOut();
+                }
+            }
+        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (mFirebaseAuth.getCurrentUser() != null){
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_LONG).show();
+                    //signed in
+                } else {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setProviders(
+                                    AuthUI.EMAIL_PROVIDER
+                                    , AuthUI.GOOGLE_PROVIDER
+                            )
+                            .build(),
+                            RC_SIGN_IN
+                    );
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
